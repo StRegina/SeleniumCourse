06541a051d4a961270fb2848e2a1505e8f61e7ed
Regina <st.reginaaa@gmail.com>
2021-03-24 20:35:06 +0500

    Adding a project

---
+++ test_main_page.py
@@ -0,0 +1,8 @@
+# git init → git add . → git status → git commit -m "write your message" - запушить в git
+# pytest -v --tb=line --language=en ~/PycharmProjects/SeleniumCourse/test_main_page.py - --tb=line, которая указывает, что нужно выводить только одну строку из лога каждого упавшего теста.
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
