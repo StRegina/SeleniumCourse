cfbce2c2343576b24a511ec701cd53c9f35abeaa
Regina <st.reginaaa@gmail.com>
2021-03-24 20:36:46 +0500

    Adding a project

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-# git init → git add . → git status → git commit -m "write your message" - запушить в git
+# git init → git add . → git status → git commit -m "Adding a project" - запушить в git
 # pytest -v --tb=line --language=en ~/PycharmProjects/SeleniumCourse/test_main_page.py - --tb=line, которая указывает, что нужно выводить только одну строку из лога каждого упавшего теста.
 
 def test_guest_can_go_to_login_page(browser):
