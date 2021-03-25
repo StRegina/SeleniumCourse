e7c31cab3db82fdb18e25e3d70c9786b1b393557
Regina <st.reginaaa@gmail.com>
2021-03-25 18:38:17 +0500

    Первый тест на основе Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 # git init → git add . → git status → git commit -m "Adding a project" - запушить в git
 # pytest -v --tb=line --language=en ~/PycharmProjects/SeleniumCourse/test_main_page.py - --tb=line, которая указывает, что нужно выводить только одну строку из лога каждого упавшего теста.
+from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
